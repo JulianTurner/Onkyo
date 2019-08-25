1db434894b44ca44f3e7610a0fecf196b95aa9b0
Julian <tuerner.j@gmail.com>
2019-08-25 10:11:20 +0200

    basic client wich listens to given IP and Port

--- Onkyo.js
+++ Onkyo.js
@@ -1,10 +1,23 @@
-//discover reciver
+var net = require('net');
+var OnkyoPort = 60128;
+var OnkyoIp = '192.168.178.25';
 
-//connect to reciver
+//create new client and bind to socket
+var client = new net.Socket();
 
-//listen to recicver
+//connect the client
+client.connect(OnkyoPort, OnkyoIp, function(){
+    console.log('connected');
+})
 
-//send data
+//fires when data is recived
+client.on('data', function(data) {
+	console.log('Received: ' + data);
+    //close connection
+    client.destroy;
+});
 
-//recive data
 
+client.on('close', function() {
+	console.log('Connection closed');
+});
